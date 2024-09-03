85fc6c744438b1e56349b12aee8eacc6ccb9e98b
lalaDevil <elainemg14@gmail.com>
2024-09-03 11:55:32 -0300

    Initial commit

---
+++ src/main/java/me/dio/DioDevWeek2024Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioDevWeek2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioDevWeek2024Application.class, args);
+	}
+
+}
