542e0c7f1a454e998e8e435861e7b8e6dc359063
lalaDevil <elainemg14@gmail.com>
2024-09-04 14:29:23 -0300

    Implementação da camada de domínio

--- src/main/java/me/dio/SantanderDevWeek2023Application.java
+++ src/main/java/me/dio/SantanderDevWeek2023Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DioDevWeek2024Application {
+public class SantanderDevWeek2023Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DioDevWeek2024Application.class, args);
+		SpringApplication.run(SantanderDevWeek2023Application.class, args);
 	}
 
 }
